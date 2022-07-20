cd49b0591a48d23cccc079ae3281edf0984589fe
Haley Telega <haleytelega@outlook.com>
2022-07-20 13:42:33 -0400

    added code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,7 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { Category } = require('../models');
-
 const resolvers = {
   Query: {
-    categories: async () => {
-      return await Category.find();
+    helloWorld: () => {
+      return "hello world";
     },
   },
 };
