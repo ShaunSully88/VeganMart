45c6502968502d1aa1bb87bc6eaaba75a6e5bd92
Eric Kohler <erickohler1600@gmail.com>
2022-07-24 13:50:22 -0400

    setup appjs, components and util functions

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-
-export const QUERY_CATEGORIES = gql`
-  {
-    categories {
-      _id
-      name
-    }
-  }
-`;
-
 export const QUERY_PRODUCTS = gql`
   query getProducts($category: ID) {
     products(category: $category) {
@@ -33,11 +16,55 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
-export const QUERY_ORDER = gql`
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
   {
     categories {
       _id
       name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
     }
   }
 `;
