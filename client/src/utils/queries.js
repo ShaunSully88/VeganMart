23e2a3e33063bbc08eaed0e85208d732895b0fbb
Eric Kohler <erickohler1600@gmail.com>
2022-07-24 13:17:56 -0400

    setup frontend react dependencies and connect index.html to index.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,43 @@
+import { gql } from '@apollo/client';
+
 export const QUERY_CHECKOUT = gql`
   query getCheckout($products: [ID]!) {
     checkout(products: $products) {
       session
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_ORDER = gql`
+  {
+    categories {
+      _id
+      name
     }
   }
 `;
