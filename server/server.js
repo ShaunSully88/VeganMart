c9c47e646ca695630a15a846be2739d2b3634126
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-27 15:52:24 -0400

    editing app.use

--- server/server.js
+++ server/server.js
@@ -16,15 +16,14 @@ const server= new ApolloServer({
 });
 
 const app = express(express.urlencoded({ extended: true }));
-app.use(express.json());
 
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 async function startApolloServer(typeDefs, resolvers) {
   await server.start();
   server.applyMiddleware({ app });
