82cb0824c4e79f81f0f67ff8eb7c33a7dc0b32ad
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-27 15:53:01 -0400

    editing app.get

--- server/server.js
+++ server/server.js
@@ -21,9 +21,12 @@ app.use('/images', express.static(path.join(__dirname, '../client/images')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
+
 async function startApolloServer(typeDefs, resolvers) {
   await server.start();
   server.applyMiddleware({ app });
