ff9cfa141244e60a467c22782386d6886d872a88
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-26 21:03:39 -0400

    update

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const { authMiddleware } = require("./utils/auth");
 
-const server = new ApolloServer({
+const server= new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
